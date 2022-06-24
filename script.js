7e3d83f7eb140d53198f4043ff4ba4643f7ebf77
Oscar Dominguez <dominguez.celada@gmail.com>
2022-06-25 00:23:56 +0200

    feat: initial version

---
+++ script.js
@@ -0,0 +1,9 @@
+// @ts-check
+
+/**
+ * Migrate occurrences of 'pika build' to 'pika-pack build' in package.json for all the repositories of a GitHub Organization
+ *
+ * @param {import('@octoherd/cli').Octokit} octokit
+ * @param {import('@octoherd/cli').Repository} repository
+ */
+export async function script(octokit, repository) {}
